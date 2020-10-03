5e0295709d8f84b3428c3d70ce3b083a0f8d3424
C36-HussainHaidarAlmousawi <70634196+C36-HussainHaidarAlmousawi@users.noreply.github.com>
2020-10-03 16:59:14 +0300

    done

--- SpeedTest5/ViewController.swift
+++ SpeedTest5/ViewController.swift
@@ -2,25 +2,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBAction func red() {
+    @IBAction func Red(_ sender: Any) {
         view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        
+    }
+    @IBAction func Blue(_ sender: Any) {
+        view.backgroundColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
     }
     
-    @IBAction func blue() {
-        view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+    @IBAction func Yellow(_ sender: Any) {
+        view.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
     }
     
-    
-    
-    @IBAction func yellow() {
-        view.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+    @IBAction func Green(_ sender: Any) {
+        view.backgroundColor = #colorLiteral(red: 0, green: 0.7801020741, blue: 0.258882165, alpha: 1)
     }
-    
-    @IBAction func green() {
-        view.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
-    }
-    
     
 }
 
